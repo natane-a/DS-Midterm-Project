97569e0f1e349285604fe50ab09ddd512c00c000
Kai Liu <liukai1919@gmail.com>
2024-11-04 19:00:08 -0800

    EDA update

--- notebooks/functions_variables.py
+++ notebooks/functions_variables.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def encode_tags(df):
+def encode_tags(df, freq_threshold=1000):
 
     """Use this function to manually encode tags from each sale.
     You could also provide another argument to filter out low 
@@ -12,14 +12,16 @@ def encode_tags(df):
     Returns:
         pandas.DataFrame: modified with encoded tags
     """
-    unique_tags = set()
+    unique_tags = {}
     for tags in df['tags']:
         if isinstance(tags, list):
-            unique_tags.update(tags)
+            for tag in tags:
+                unique_tags[tag] = unique_tags.get(tag, 0) +1
     
     new_columns = {}
-    for tag in unique_tags:
-        new_columns[tag] = df['tags'].apply(lambda x: 1 if isinstance(x, list) and tag in x else 0)
+    for tag in unique_tags.keys():
+        if unique_tags[tag] > freq_threshold:
+            new_columns[tag] = df['tags'].apply(lambda x: 1 if isinstance(x, list) and tag in x else 0)
     
     new_df = pd.DataFrame(new_columns)
     df = pd.concat([df, new_df], axis=1)
